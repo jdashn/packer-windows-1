fede65ad0939d97fc106b9b04f6990dc06d9553e
skylock <emanuil_usa@yahoo.com>
2015-10-01 15:43:33 +0300

    Updated installation script for vmware tools v.10 (vmware workstation pro 12)

--- debug_win2012r2-standard.bat
+++
@@ -1,4 +0,0 @@
-SET PACKER_LOG=enable
-SET PACKER_LOG_PATH=d:\Packer\packer-windows\packer_log_win2012r2-standard.txt
-::packer build win2012r2-standard.json 
-packer build -debug win2012r2-standard.json 

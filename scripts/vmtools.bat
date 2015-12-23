2eb70c4f44195316046883a4b1839375dc6ef63a
skylock <emanuil_usa@yahoo.com>
2015-12-23 23:22:24 +0200

    update tools to 10.0.5 - included in workstation pro 12.1

--- scripts/vmtools.bat
+++ scripts/vmtools.bat
@@ -6,7 +6,7 @@ if not defined PACKER_SEARCH_PATHS set PACKER_SEARCH_PATHS="%USERPROFILE%" a: b:
 if not defined SEVENZIP_32_URL set SEVENZIP_32_URL=http://www.7-zip.org/a/7z938.msi
 if not defined SEVENZIP_64_URL set SEVENZIP_64_URL=http://www.7-zip.org/a/7z938-x64.msi
 if not defined VBOX_ISO_URL set VBOX_ISO_URL=http://download.virtualbox.org/virtualbox/4.3.28/VBoxGuestAdditions_4.3.28.iso
-if not defined VMWARE_TOOLS_TAR_URL set VMWARE_TOOLS_TAR_URL=https://softwareupdate.vmware.com/cds/vmw-desktop/ws/12.0.0/2985596/windows/packages/tools-windows.tar
+if not defined VMWARE_TOOLS_TAR_URL set VMWARE_TOOLS_TAR_URL=https://softwareupdate.vmware.com/cds/vmw-desktop/ws/12.1.0/3272444/windows/packages/tools-windows.tar
 goto main
 
 ::::::::::::
